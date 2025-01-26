ce06880d526f5ca386dbe5bcc6a1e0595259da65
Jack Wright <jackwright@jacks-mac-mini.tailc1e603.ts.net>
2025-01-26 15:32:31 +0000

    add backward pass example

---
+++ basic-backward-pass-example.py
@@ -0,0 +1,34 @@
+import torch as t
+
+
+def from_pytorch():
+    input = t.tensor(1)
+    expected = t.tensor(2)
+    weight = t.tensor(1.0, requires_grad=True)
+
+    prediction = weight * input
+    loss = (prediction - expected) ** 2
+
+    print(loss)
+
+    loss.backward()
+    print(weight.grad)
+
+
+def from_scratch():
+    input = 1
+    expected = 2
+    weight = 1.0
+
+    prediction = weight * input
+    loss = (prediction - expected) ** 2
+
+    print(loss)
+
+    # Compute the gradient
+    backward_pass_grad = 2 * (prediction - expected) * input
+
+    print(backward_pass_grad)
+
+from_pytorch()
+from_scratch()
