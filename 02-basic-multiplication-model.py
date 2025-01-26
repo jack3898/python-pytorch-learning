db55e7ce09edb2e2b3ee5c55a53eb24eec72b05e
Jack Wright <jackwright@jacks-mac-mini.tailc1e603.ts.net>
2025-01-26 16:35:46 +0000

    add pytorch example to basic multiplication model module

--- 02-basic-multiplication-model.py
+++ 02-basic-multiplication-model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 # This is a simple model that multiplies the input by a weight
 # and tries to predict the expected output. As each epoch passes, 
@@ -39,4 +40,42 @@ def from_scratch():
     print("Final weight: ", weight)
     print("Final prediction: ", forward(input_arr))
 
+
+def from_pytorch():
+    input_tensor = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
+    expected_tensor = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
+
+    weight = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
+
+    def forward(input):
+        return weight * input
+
+    def loss(expected, prediction):
+        return ((prediction-expected) ** 2).mean()
+
+    print("Initial weight: ", weight)
+    print("Initial prediction: ", weight * input_tensor)
+
+    epochs = 100
+    learning_rate = 0.01
+
+    for epoch in range(epochs):
+        prediction = forward(input_tensor)
+        loss_val = loss(expected_tensor, prediction)
+        
+        loss_val.backward()
+
+        with torch.no_grad():
+            weight -= learning_rate * weight.grad
+
+        weight.grad.zero_()
+
+        if epoch % 2 == 0:
+            print(f"Epoch: {epoch}, Loss: {loss_val}, Weight: {weight}")
+ 
+    print("Final weight: ", weight)
+    print("Final prediction: ", forward(input_tensor))
+
+
 from_scratch()
+from_pytorch()
